d31b9a0e73056b88fc8d6c4d5b2c7f4647af4514
adjelritmo <renionapp@gmail.com>
2025-05-26 21:57:25 +0100

    Adicioneni o env

--- models/db.js
+++ models/db.js
@@ -1,10 +1,20 @@
-require('dotenv/config')
-const Sequelize = require('sequelize')
+require('dotenv').config() // Carrega as variáveis do .env
+const  Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
+// Verifica se a variável de ambiente existe
+if (!process.env.DB_URL) {
+  throw new Error('Variável de ambiente DB_URL não está definida')
+}
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 module.exports = {
