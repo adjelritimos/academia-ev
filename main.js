47fddb37651d4877834ffab62212ed346c08f442
adjelritmo <renionapp@gmail.com>
2025-05-27 07:11:05 +0100

    Adicioneni o env host

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+require('dotenv/config')
 const express = require('express')
 const app = express()
-const port = 5349
+const port = process.env.PORT || 5349
 const admin = require('./routes/admin')
 const user = require('./routes/user')
 const cors = require("cors")
