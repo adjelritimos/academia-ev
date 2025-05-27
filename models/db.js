6237af95b17c63dadc4c1f78ade5e521b2cccae9
adjelritmo <renionapp@gmail.com>
2025-05-27 07:02:01 +0100

    Adicioneni o env host

--- models/db.js
+++ models/db.js
@@ -3,7 +3,8 @@ const  Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  port: process.env.PORT
+  port: process.env.PORT,
+  host: process.env.DB_HOST,
 })
 
 module.exports = {
